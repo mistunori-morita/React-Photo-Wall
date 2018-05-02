dec3a52631d1e6cb5805d1c18644e41890966542
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-05-02 19:14:28 +0900

    react App setting

---
+++ src/Comonents/Photo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+
+class Photo extends Component {
+  render(){
+    const post = this.props.post
+
+    return <figure className="figure"></figure>
+  }
+}
+
+
+export default Photo
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Main from './Comonents/Main'
+import './styles/style.css'
 
 ReactDOM.render(<Main/>,document.getElementById('root'));
