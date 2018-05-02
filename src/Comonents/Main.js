fba21b9130369455fd547dcd6ea287022e2f0752
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-05-02 18:42:52 +0900

    first-commit

---
+++ src/Comonents/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import Title from './Title'
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Title title="photowall" />
+      </div>
+    )
+  }
+}
+
+
+export default Main;
