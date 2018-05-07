1b8721aea8a15fb7ed445278e1a8859e1108d814
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-05-07 19:54:03 +0900

    Pass image to component and display it

--- src/Comonents/Photo.js
+++ src/Comonents/Photo.js
@@ -6,7 +6,15 @@ class Photo extends Component {
   render(){
     const post = this.props.post
 
-    return <figure className="figure"></figure>
+    return <figure className="figure">
+      <img className="photo" src={post.imageLink} alt={post.description}/>
+      <figcaption>
+        <p>{post.description}</p>
+        <div className="btn-container">
+          <button className="remove-button">Remove</button>
+        </div>
+      </figcaption>
+    </figure>
   }
 }
 
