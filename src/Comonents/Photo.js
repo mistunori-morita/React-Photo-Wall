eef29654518a4399cee1cbc4224480e55d003cb9
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-05-08 19:18:58 +0900

    update state

--- src/Comonents/Photo.js
+++ src/Comonents/Photo.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 function Photo(props){
   const post = props.post
-
   return <figure className="figure">
         <img className="photo" src={post.imageLink} alt={post.description}/>
         <figcaption>
           <p>{post.description}</p>
           <div className="btn-container">
-            <button className="remove-button">Remove</button>
+            <button className="remove-button" onClick={ () => {
+              props.onRemovePhoto(post)
+            }}>Remove</button>
           </div>
         </figcaption>
       </figure>
