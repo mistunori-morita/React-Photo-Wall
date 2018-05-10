23f4c3c60f3c81ac49d03134e9e1a9f468ef5ec7
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-05-10 13:54:52 +0900

    React-router-dom Link setup

--- src/Comonents/PhotoWall.js
+++ src/Comonents/PhotoWall.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import Photo from './Photo'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+
 
 function PhotoWall(props){
   return <div className="PhotoWallWrap">
-          <a className="addIcon" onClick={props.onNavigate} href="#AddPhoto">Click Me</a>
+          <Link className="addIcon" to="/AddPhoto">Click Me</Link>
           {/* propsで渡ってきたが、main.jsでthisをbindしてないとundefinedになって参照できない */}
-          {/* <button className="addIcon" onClick={props.onNavigate}>+</button> */}
           <div className="photoGrid">
             {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
           </div>
