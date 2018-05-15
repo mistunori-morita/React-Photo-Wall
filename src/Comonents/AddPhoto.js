45cf5bfc86706e93be29d580188c9a66a294bf52
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-05-15 19:27:06 +0900

    AddPhoto component styling

--- src/Comonents/AddPhoto.js
+++ src/Comonents/AddPhoto.js
@@ -5,7 +5,16 @@ import React, { Component } from 'react'
 class AddPhoto extends Component{
   render(){
     return(
-      <h1> this is the page where we will add photos</h1>
+      <div>
+        <h1> this is the page where we will add photos</h1>
+         <div className="form">
+          <form>
+            <input type="text" placeholder="Link"/>
+            <input type="text" placeholder="Description"/>
+            <button class="Add-Button">Post</button>
+          </form>
+        </div>
+      </div>
     )
   }
 }
