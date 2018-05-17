ea65a5cc8c1ab6667615897a76cb6fdf8143c808
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-05-17 12:27:15 +0900

    Redux setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import Main from './Comonents/Main'
 import './styles/style.css'
 import {BrowserRouter} from 'react-router-dom'
+import {createStore} from 'redux'
+
+const store = createStore()
 
 ReactDOM.render(
   <BrowserRouter>
